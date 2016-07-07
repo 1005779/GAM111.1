abc653fc6f4c057d01f69cdd77ef1087bee3c870
1005779 <t.quick@live.com.au>
2016-07-07 18:30:25 +1000

    nearly implemented the ballistics

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-    public float moveSpeed = 30.0f;
     public float lifeTime = 30.0f;
     public float damage = 100.0f;
 
@@ -12,14 +11,16 @@ public class Projectile : MonoBehaviour {
     public float X;
     public float Y;
     public float launchAngel;
-    public float gavity;
-    public Vector3 currentVector;
+    public float gravity = 9.807f;
     public Vector3 LaunchAngel;
     public GameObject Barrel;
+    public float moveSpeed = 30.0f;
 
 	// Use this for initialization
 	void Start () {
-        
+        //launchAngel
+        LaunchAngel = Barrel.transform.rotation;
+	
 	
 	}
 	
@@ -34,14 +35,16 @@ public class Projectile : MonoBehaviour {
     public void BalisticVariables()
     {
         X = this.gameObject.transform.position.x;
-        Y = this.gameObject.transform.position.Y;
-        //launchAngel
+        Y = this.gameObject.transform.position.y;
+        
+        
+
 
     }
 
     public void Move()
     {
-        
+        this.transform.Translate(X * Mathf.Tan(LaunchAngel)-((gravity * X) / (Mathf.Pow((moveSpeed * Mathf.Cos(LaunchAngel)), 2)));  
         //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
     }
 
